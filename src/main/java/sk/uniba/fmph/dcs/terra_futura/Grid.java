b8ef53d3e71698ee0eafc961f8635fb9998450a4
MarkKucher <markkucher100@gmail.com>
2025-11-19 19:19:18 +0100

    MoveCard draft implementation along with few interfaces needed.

---
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Grid.java
@@ -0,0 +1,14 @@
+package sk.uniba.fmph.dcs.terra_futura;
+
+import java.util.Optional;
+
+public interface Grid {
+    Optional<Card> getCard(GridPosition coordinate);
+    boolean canPutCard(GridPosition coordinate);
+    void putCard(GridPosition coordinate, Card card);
+    boolean canBeActivated(GridPosition coordinate);
+    void setActivated(GridPosition coordinate);
+    void setActivationPattern(ActivationPattern pattern);
+    void endTurn();
+    String state();
+}
