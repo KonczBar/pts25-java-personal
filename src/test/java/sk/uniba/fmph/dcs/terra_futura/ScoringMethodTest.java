ece3cfb23ed83e204a0fe1f1bd45a7b8e2a8a562
Curse <curse@thinkpad.localdomain>
2025-11-25 16:18:58 +0100

    Fixed broken tests after merge with main, added mockito for testing

--- src/test/java/sk/uniba/fmph/dcs/terra_futura/ScoringMethodTest.java
+++ src/test/java/sk/uniba/fmph/dcs/terra_futura/ScoringMethodTest.java
@@ -2,6 +2,9 @@ package sk.uniba.fmph.dcs.terra_futura;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.util.*;
 
@@ -9,44 +12,8 @@ import static org.junit.Assert.*;
 import static sk.uniba.fmph.dcs.terra_futura.Resource.CAR;
 import static sk.uniba.fmph.dcs.terra_futura.Resource.GREEN;
 
-class GridFake implements Grid {
-
-    @Override
-    public Optional<Card> getCard(GridPosition coordinate) {
-        if (coordinate.getX() < 0 || coordinate.getY() < 0) {
-            return Optional.empty();
-        }
-
-        if (coordinate.getX() == 0) {
-            return Optional.of(new PollutedCardFake());
-        }
-
-        return Optional.of(new ResourcesCardFake());
-    }
-}
-
-class PollutedCardFake implements Card {
-
-    @Override
-    public boolean isPolluted() {
-        return true;
-    }
-
-    @Override
-    public Map<Resource, Integer> actuallyGetResources() {
-        throw new RuntimeException();
-    }
-}
-
-class ResourcesCardFake implements Card {
-
-    @Override
-    public boolean isPolluted() {
-        return false;
-    }
-
-    @Override
-    public Map<Resource, Integer> actuallyGetResources() {
+class ResourcesCardFake {
+    public static Map<Resource, Integer> actuallyGetResources() {
         Map<Resource, Integer> result = new HashMap<>();
         for (Resource resource : Resource.values()) {
             result.put(resource, 1);
@@ -55,30 +22,58 @@ class ResourcesCardFake implements Card {
     }
 }
 
+
 public class ScoringMethodTest {
 
-    private ScoringMethod scoringMethod;
+    private ScoringMethod scoringMethod1;
+    private ScoringMethod scoringMethod2;
 
     @Before
     public void setUp() {
+        Card pollutedCardFake = Mockito.mock(Card.class);
+        Card resourceCardFake = Mockito.mock(Card.class);
+        Grid gridFake1 = Mockito.mock(Grid.class);
+        Grid gridFake2 = Mockito.mock(Grid.class);
+
+        Mockito.when(pollutedCardFake.isPolluted()).thenReturn(true);
+        Mockito.when(resourceCardFake.isPolluted()).thenReturn(false);
+        Mockito.when(pollutedCardFake.actuallyGetResources()).thenThrow(new RuntimeException());
+        Mockito.when(resourceCardFake.actuallyGetResources()).thenReturn(ResourcesCardFake.actuallyGetResources());
+        Mockito.when(gridFake1.getCard(Mockito.any())).thenReturn(Optional.of(pollutedCardFake));
+        Mockito.when(gridFake2.getCard(Mockito.any())).thenReturn(Optional.of(resourceCardFake));
+
         List<Resource> resources = new ArrayList<>();
         resources.add(GREEN);
         resources.add(GREEN);
         resources.add(CAR);
-        scoringMethod = new ScoringMethod(resources, 5, new GridFake());
+        scoringMethod1 = new ScoringMethod(resources, 5, gridFake1);
+        scoringMethod2 = new ScoringMethod(resources, 5, gridFake2);
     }
 
     @Test
-    public void ScoringTest() {
+    public void ScoringTest1() {
         System.out.println("State before calculation:");
-        System.out.println(scoringMethod.state());
-        assertFalse(scoringMethod.getCalculatedTotal().isPresent());
+        System.out.println(scoringMethod1.state());
+        assertFalse(scoringMethod1.getCalculatedTotal().isPresent());
 
         System.out.println("State after calculation:");
-        scoringMethod.selectThisMethodAndCalculate();
-        System.out.println(scoringMethod.state());
-        assertTrue(scoringMethod.getCalculatedTotal().isPresent());
-        assertEquals(Optional.of(120), scoringMethod.getCalculatedTotal());
+        scoringMethod1.selectThisMethodAndCalculate();
+        System.out.println(scoringMethod1.state());
+        assertTrue(scoringMethod1.getCalculatedTotal().isPresent());
+        assertEquals(Optional.of(-25), scoringMethod1.getCalculatedTotal());
+    }
+
+    @Test
+    public void ScoringTest2() {
+        System.out.println("State before calculation:");
+        System.out.println(scoringMethod2.state());
+        assertFalse(scoringMethod2.getCalculatedTotal().isPresent());
+
+        System.out.println("State after calculation:");
+        scoringMethod2.selectThisMethodAndCalculate();
+        System.out.println(scoringMethod2.state());
+        assertTrue(scoringMethod2.getCalculatedTotal().isPresent());
+        assertEquals(Optional.of(510), scoringMethod2.getCalculatedTotal());
     }
 
 }
