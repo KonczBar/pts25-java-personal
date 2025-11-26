809dac2bb1953d010a666188b1ef8590bd189115
Igor <travenec2@uniba.sk>
2025-11-26 18:50:19 +0100

    implemented EffectArbitraryBasic and simplified code

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/Effect.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Effect.java
@@ -1,10 +1,23 @@
 package sk.uniba.fmph.dcs.terra_futura;
 
 import java.util.Map;
+import java.util.Objects;
+
 
 public interface Effect {
     boolean check(Map<Resource, Integer> input, Map<Resource, Integer> output, int pollution);
     boolean hasAssistance();
     String state();
 
+    public static boolean checkResource(Resource resource, Map<Resource, Integer> effectMap, Map<Resource, Integer> map){
+        if (!map.containsKey(resource)){
+            return effectMap.get(resource) <= 0;
+        }
+        else{
+            if (resource == Resource.POLLUTION){
+                throw new InvalidMoveException("Pollution in output - put it in pollution int");
+            }
+            return Objects.equals(effectMap.get(resource), map.get(resource));
+        }
+    }
 }
--- src/main/java/sk/uniba/fmph/dcs/terra_futura/EffectArbitraryBasic.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/EffectArbitraryBasic.java
@@ -6,34 +6,39 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EffectTransformationFixed implements Effect{
-    private final Map<Resource, Integer> input;
+public class EffectArbitraryBasic implements Effect{
+    private final int input;
     private final Map<Resource, Integer> output;
     private final int pollution;
 
-    public EffectTransformationFixed(Map<Resource, Integer> input, Map<Resource, Integer> output, int pollution){
-        this.input = new HashMap<>(input);
+    public EffectArbitraryBasic(int input, Map<Resource, Integer> output, int pollution){
         this.output = new HashMap<>(output);
         for (Resource resource : Resource.values()){
-            if (!input.containsKey(resource)){
-                this.input.put(resource, 0);
-            }
             if (!output.containsKey(resource)){
                 this.output.put(resource, 0);
             }
         }
+        this.input = input;
         this.pollution = pollution;
     }
 
     @Override
     public boolean check(Map<Resource, Integer> input, Map<Resource, Integer> output, int pollution) {
+        int inputAmount = 0;
         for (Resource resource : Resource.values()){
-            if (!Effect.checkResource(resource, this.input, input)){
-                return false;
+            if (input.containsKey(resource)){
+                if (input.get(resource) > 0 && resource != Resource.GREEN && resource != Resource.RED && resource != Resource.YELLOW){
+                    return false;
+                }
+                inputAmount += input.get(resource);
             }
+
             if (!Effect.checkResource(resource, this.output, output)){
                 return false;
             }
+        }
+        if (this.input != inputAmount){
+            return false;
         }
         return this.pollution == pollution;
     }
@@ -45,22 +50,16 @@ public class EffectTransformationFixed implements Effect{
 
     @Override
     public String state() {
-        JSONArray effectInput = new JSONArray();
         JSONArray effectOutput = new JSONArray();
         for (Resource resource : Resource.values()) {
             JSONObject pair = new JSONObject();
             pair.put("Resource", resource);
-            int amount = input.getOrDefault(resource, 0);
-            pair.put("Amount", amount);
-            effectInput.put(pair);
-            pair = new JSONObject();
-            pair.put("Resource", resource);
-            amount = output.getOrDefault(resource, 0);
+            int amount = output.getOrDefault(resource, 0);
             pair.put("Amount", amount);
             effectOutput.put(pair);
         }
         JSONObject result = new JSONObject();
-        result.put("Input", effectInput);
+        result.put("Input", input);
         result.put("Output", effectOutput);
         result.put("Pollution", pollution);
         return result.toString();
