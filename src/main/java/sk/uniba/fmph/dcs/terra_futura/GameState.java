e30b201c8f1bcfd97b844f1a3c84e5c185527d6c
Curse <curse@thinkpad.localdomain>
2025-11-28 23:50:41 +0100

    Removed unused GameState: TakeCardCardActivated

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/GameState.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/GameState.java
@@ -3,7 +3,6 @@ package sk.uniba.fmph.dcs.terra_futura;
 public enum GameState {
     TakeCardNoCardDiscarded,
     TakeCardCardDiscarded,
-    TakeCardCardActivated,
     ActivateCard,
     SelectReward,
     SelectActivationPattern,
