0534c3eb15f6a2bebafbda54e2ddee5028ef650e
MatsFedir <matsiutsia.fedir@gmail.com>
2025-11-28 23:57:19 +0100

    Created GameObserver with unit tests. Added folder for exceptions.

---
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Exceptions/InvalidObserverException.java
@@ -0,0 +1,7 @@
+package sk.uniba.fmph.dcs.terra_futura.Exceptions;
+
+public class InvalidObserverException extends RuntimeException {
+    public InvalidObserverException(String message) {
+        super(message);
+    }
+}
