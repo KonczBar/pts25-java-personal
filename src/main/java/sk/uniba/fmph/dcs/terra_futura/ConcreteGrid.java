1d7b1e398a77a484fd19edc8edd3f6e61219a59c
Curse <curse@thinkpad.localdomain>
2025-11-30 12:07:02 +0100

    Implemented some grid methods

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/ConcreteGrid.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/ConcreteGrid.java
@@ -1,31 +1,136 @@
 package sk.uniba.fmph.dcs.terra_futura;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.apache.commons.lang3.math.NumberUtils.max;
+import static org.apache.commons.lang3.math.NumberUtils.min;
+
 public class ConcreteGrid implements Grid {
-    @Override
-    public Optional<Card> getCard(GridPosition coordinate) {
-        return Optional.empty();
+    List<List<Optional<Card>>> cardMatrix;
+    List<List<Boolean>> activatable;
+    int cardCount = 0;
+    int minX = 2;
+    int maxX = 2;
+    int minY = 2;
+    int maxY = 2;
+    boolean onGoingActivation = false; // set to true by placing card, set to false by ending turn
+
+    public ConcreteGrid() {
+        cardMatrix = new ArrayList<>();
+        activatable = new ArrayList<>();
+
+        // initializes empty grid
+        for (int i = 0; i < 5; i++) {
+            cardMatrix.add(new ArrayList<>());
+            activatable.add(new ArrayList<>());
+
+            for (int j = 0; j < 5; j++) {
+                cardMatrix.get(i).add(Optional.empty());
+                activatable.get(i).add(false);
+            }
+        }
+
+
+    }
+
+    private void resetActivation() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                activatable.get(i).set(j, false);
+            }
+        }
     }
 
     @Override
+    public Optional<Card> getCard(GridPosition coordinate) {
+        return cardMatrix.get(coordinate.getX() + 2).get(coordinate.getY() + 2);
+    }
+
+
+    @Override
     public boolean canPutCard(GridPosition coordinate) {
-        return false;
+        // false if a card has been placed but player has yet to end turn
+        if (onGoingActivation) {
+            return false;
+        }
+
+        // first card has to be at 0, 0
+        if (cardCount == 0 && (coordinate.getY() != 0 || coordinate.getX() != 0)) {
+            return false;
+        }
+
+        // enforcing maximum 3 x 3 internal grid
+        if (coordinate.getX() + 2 < minX) {
+            if (maxX - (coordinate.getX() + 2) > 2) {
+                return false;
+            }
+        }
+
+        if (coordinate.getX() + 2 > maxX) {
+            if ((coordinate.getX() + 2) - minX > 2) {
+                return false;
+            }
+        }
+
+        if (coordinate.getY() + 2 < minY) {
+            if (maxY - (coordinate.getY() + 2) > 2) {
+                return false;
+            }
+        }
+
+        if (coordinate.getY() + 2 > maxY) {
+            if ((coordinate.getY() + 2) - minY > 2) {
+                return false;
+            }
+        }
+
+        return cardMatrix.get(coordinate.getX() + 2).get(coordinate.getY() + 2).isEmpty();
     }
 
     @Override
     public void putCard(GridPosition coordinate, Card card) {
+        if (!canPutCard(coordinate)) {
+            throw new IllegalArgumentException("Card already present or index out of range");
+        }
+
+        cardMatrix.get(coordinate.getX() + 2).set(coordinate.getY() + 2, Optional.of(card));
+        onGoingActivation = true;
+
+        // cards now activatalbe in cross pattern
+        for (int i = 0; i < 5; i++) {
+            activatable.get(coordinate.getX() + 2).set(i, true);
+            activatable.get(i).set(coordinate.getY() + 2, true);
+        }
+
+        maxX = max(maxX, coordinate.getX() + 2);
+        minX = min(minX, coordinate.getX() + 2);
+        maxY = max(maxY, coordinate.getY() + 2);
+        minY = min(minY, coordinate.getY() + 2);
+
+        if (maxX - minX > 2 || maxY - minY > 2) {
+            throw new RuntimeException("Cards occupy too much space:\n" + state());
+        }
+
 
     }
 
     @Override
     public boolean canBeActivated(GridPosition coordinate) {
-        return false;
+        // returns true if the card is in a row/column where a card has been placed
+        // AND a card is present at the coordinate
+        return (!activatable.get(coordinate.getX() + 2).get(coordinate.getY() + 2) &&
+                cardMatrix.get(coordinate.getX() + 2).get(coordinate.getY() + 2).isPresent());
     }
 
     @Override
     public void setActivated(GridPosition coordinate) {
+        if (!canBeActivated(coordinate)) {
+            throw new IllegalArgumentException("This card cannot be activated");
+        }
 
+        activatable.get(coordinate.getX() + 2).set(coordinate.getY() + 2, false);
     }
 
     @Override
@@ -35,7 +140,8 @@ public class ConcreteGrid implements Grid {
 
     @Override
     public void endTurn() {
-
+        onGoingActivation = false;
+        resetActivation();
     }
 
     @Override
