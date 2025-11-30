1495e452bbeb7b01b954496669c89434c9a7940d
Curse <curse@thinkpad.localdomain>
2025-11-30 20:41:09 +0100

    Untested SelectReward class

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/SelectReward.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/SelectReward.java
@@ -1,25 +1,57 @@
 package sk.uniba.fmph.dcs.terra_futura;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class SelectReward {
-    private Optional<Integer> player;
-    private List<Resource> selection;
+    private Optional<Integer> player = Optional.empty();
+    private List<Resource> selection = new ArrayList<>();
+    private Card card;
+    private boolean done = false;
 
     public void setReward(int player, Card card, List<Resource> reward) {
-        return;
+        this.player = Optional.of(player);
+        this.selection = new ArrayList<>(reward);
+        this.card = card;
     }
 
     public boolean canSelectReward(Resource resource) {
-        return false;
+        // nonexistent player can't select reward
+        if (player.isEmpty()) {
+            return false;
+        }
+
+        // cannot select a resource that isn't available
+        if (!selection.contains(resource)) {
+            return false;
+        }
+
+        // cannot select reward twice
+        if (done) {
+            return false;
+        }
+
+        // cannot put resource on overpolluted card
+        Map<Resource, Integer> request = new HashMap<>();
+        request.put(resource, 1);
+        if (!card.canPutResources(request)) {
+            return false;
+        }
+
+        return true;
     }
 
-    public void selectReward() {
-        return;
+    public void selectReward(Resource resource) {
+        if (!canSelectReward(resource)) {
+            throw new IllegalArgumentException("Resource not available: " + resource);
+        }
+
+        Map<Resource, Integer> request = new HashMap<>();
+        request.put(resource, 1);
+        card.putResources(request);
+        done = true;
     }
 
     public String state() {
-        return "";
+        return "Player: " + player + " Selection: " + selection + " Done: " + done;
     }
 }
